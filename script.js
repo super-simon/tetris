2c4ad0c29e3221e567e3c21606d948af599e1f02
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-02-21 14:25:34 +0200

    first figure

---
+++ script.js
@@ -0,0 +1,117 @@
+// 1. Додати нові фігури
+// 2. Стилізувати нові фігури
+// 3. Додати функцію рандому котра буде повертати випадкову фігуру
+// 4. Центрувати фігуру незалежно від ширини фігури
+
+const PLAYFIELD_COLUMNS = 10;
+const PLAYFIELD_ROWS = 20;
+const TETROMINO_NAMES = ["O", "J"];
+const TETROMINOES = {
+  O: [
+    [1, 1],
+    [1, 1],
+  ],
+  J: [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+};
+
+function convertPositionToIndex(row, column) {
+  return row * PLAYFIELD_COLUMNS + column;
+}
+
+let playfield;
+let tetromino;
+
+function generatePlayField() {
+  for (let i = 0; i < PLAYFIELD_ROWS * PLAYFIELD_COLUMNS; i++) {
+    const div = document.createElement("div");
+    document.querySelector(".grid").append(div);
+  }
+
+  playfield = new Array(PLAYFIELD_ROWS).fill(
+    new Array(PLAYFIELD_COLUMNS).fill(0)
+  );
+  // console.log(playfield);
+}
+
+function generateTetromino() {
+  const name = TETROMINO_NAMES[1];
+  const matrix = TETROMINOES[name];
+
+  tetromino = {
+    name,
+    matrix,
+    row: 4,
+    column: 3,
+  };
+}
+
+generatePlayField();
+generateTetromino();
+const cells = document.querySelectorAll(".grid div");
+
+function drawPlayField() {
+  for (let row = 0; row < PLAYFIELD_ROWS; row++) {
+    for (let column = 0; column < PLAYFIELD_COLUMNS; column++) {
+      const name = playfield[row][column];
+      const cellIndex = convertPositionToIndex(row, column);
+      cells[cellIndex].classList.add(name);
+    }
+  }
+}
+
+function drawTetromino() {
+  const name = tetromino.name;
+  const tetrominoMatrixSize = tetromino.matrix.length;
+  for (let row = 0; row < tetrominoMatrixSize; row++) {
+    for (let column = 0; column < tetrominoMatrixSize; column++) {
+      if (tetromino.matrix[row][column] === 0) continue;
+      const cellIndex = convertPositionToIndex(
+        tetromino.row + row,
+        tetromino.column + column
+      );
+      cells[cellIndex].classList.add(name);
+    }
+    //column
+  }
+  //row
+}
+
+function draw() {
+  cells.forEach((cell) => cell.removeAttribute("class"));
+  drawPlayField();
+  drawTetromino();
+}
+
+draw();
+
+document.addEventListener("keydown", onkeyDown);
+function onkeyDown(e) {
+  switch (e.key) {
+    case "ArrowDown":
+      moveTetrominoDown();
+      break;
+    case "ArrowLeft":
+      moveTetrominoLeft();
+      break;
+    case "ArrowRight":
+      moveTetrominoRight();
+      break;
+  }
+  draw();
+}
+
+function moveTetrominoDown() {
+  tetromino.row += 1;
+}
+
+function moveTetrominoLeft() {
+  tetromino.column -= 1;
+}
+
+function moveTetrominoRight() {
+  tetromino.column += 1;
+}
